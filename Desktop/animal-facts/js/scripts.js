2262abab5fb2255dcd262074439730396e644695
Adam Luchini and Georgina Van Dort <georgina@>
2016-06-02 09:15:07 -0700

    include dropdown menu

--- Desktop/animal-facts/js/scripts.js
+++ Desktop/animal-facts/js/scripts.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
-  var animal = prompt("Type zebras, snakes, or spiders");
 
-  if (animal === "zebras") {
-    $('#zebras').show();
-  } else if (animal === "snakes") {
-    $('#snakes').show();
-  } else if (animal === "spiders"){
-    $('#spiders').show();
-  }
+  $("#animal").change(function(){
+    var animal = $("#animal").val();
+
+    if (animal === "zebras") {
+      $('#zebras').show();
+    } else if (animal === "snakes") {
+      $('#snakes').show();
+    } else if (animal === "spiders"){
+      $('#spiders').show();
+    }
+  })
+
+
+
+
+
+
+//  var animal = prompt("Type zebras, snakes, or spiders");
+
+//  if (animal === "zebras") {
+//    $('#zebras').show();
+//  } else if (animal === "snakes") {
+//    $('#snakes').show();
+//  } else if (animal === "spiders"){
+//    $('#spiders').show();
+//  }
 });
